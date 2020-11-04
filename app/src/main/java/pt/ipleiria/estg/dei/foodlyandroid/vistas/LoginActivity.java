c73ae69f056fedff0c2c080f3b28c17971d1c7ec
Pedro Alves <pedromiguel.alves8@gmail.com>
2020-11-04 20:57:18 +0000

    splash + "login"

--- app/src/main/java/pt/ipleiria/estg/dei/foodlyandroid/vistas/LoginActivity.java
+++ app/src/main/java/pt/ipleiria/estg/dei/foodlyandroid/vistas/LoginActivity.java
@@ -1,14 +1,16 @@
-package pt.ipleiria.estg.dei.foodlyandroid;
+package pt.ipleiria.estg.dei.foodlyandroid.vistas;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import pt.ipleiria.estg.dei.foodlyandroid.R;
+
+public class LoginActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_login);
     }
 }
