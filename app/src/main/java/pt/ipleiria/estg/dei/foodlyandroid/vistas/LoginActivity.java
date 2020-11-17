46024abe73fbf0f7f1168c570b9f257a0603a960
Pedro Alves <pedromiguel.alves8@gmail.com>
2020-11-17 17:39:58 +0000

    parte do menu

--- app/src/main/java/pt/ipleiria/estg/dei/foodlyandroid/vistas/LoginActivity.java
+++ app/src/main/java/pt/ipleiria/estg/dei/foodlyandroid/vistas/LoginActivity.java
@@ -2,15 +2,61 @@ package pt.ipleiria.estg.dei.foodlyandroid.vistas;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Patterns;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import pt.ipleiria.estg.dei.foodlyandroid.R;
 
 public class LoginActivity extends AppCompatActivity {
 
+    EditText etEmail, etPassword;
+    TextView tvEsquecer, tvRegistar;
+    Button btnLogin;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        etEmail = findViewById(R.id.editTextEmail);
+        etPassword = findViewById(R.id.editTextPassword);
+
+        btnLogin = findViewById(R.id.buttonLogin);
+    }
+
+    public void onClickLogin(View view) {
+        String email = etEmail.getText().toString();
+        String password = etPassword.getText().toString();
+
+        if(!isEmailValido(email)){
+            etEmail.setError(getString(R.string.etEmailInvalido));
+            return;
+        }
+        if(!isPasswordValida(password)){
+            etPassword.setError(getString(R.string.etPasswordInvalida));
+            return;
+        }
+
+        Intent intent = new Intent (this, MenuMainActivity.class);
+        startActivity(intent);
+    }
+
+    private boolean isEmailValido(String email){
+        if(email == null){
+            return false;
+        }
+        return Patterns.EMAIL_ADDRESS.matcher(email).matches();
+    }
+
+    private boolean isPasswordValida(String password){
+        if(password == null){
+            return false;
+        }
+        return password.length() >= 4;
     }
 }
---
+++ app/src/main/java/pt/ipleiria/estg/dei/foodlyandroid/vistas/MenuMainActivity.java
@@ -0,0 +1,16 @@
+package pt.ipleiria.estg.dei.foodlyandroid.vistas;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import pt.ipleiria.estg.dei.foodlyandroid.R;
+
+public class MenuMainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu_main);
+    }
+}
