38f3ce686e49a953b8bf4f5b6d4e74529f89c4de
PedroLealLopes <pedro.ml.lopes.98@gmail.com>
2021-02-15 20:57:05 +0000

    added IP_PC_ESCOLA to mUrlApi

--- app/src/main/java/pt/ipleiria/estg/dei/foodlyandroid/modelos/SingletonFoodly.java
+++ app/src/main/java/pt/ipleiria/estg/dei/foodlyandroid/modelos/SingletonFoodly.java
@@ -65,7 +65,7 @@ public class SingletonFoodly {
     private static final String IP_Luckdude = "192.168.1.229";
     private static final String IP_Johnny = "192.168.1.253";
     private static final String IP_PC_ESCOLA = "";
-    private static final String mUrlAPI = "http://" + IP_MiiTU + "/FoodlyWeb/frontend/web/api";
+    private static final String mUrlAPI = "http://" + IP_PC_ESCOLA + "/FoodlyWeb/frontend/web/api";
 
     public static synchronized SingletonFoodly getInstance(Context context) {
         if (instance == null)
